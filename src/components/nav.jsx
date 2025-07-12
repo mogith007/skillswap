a1576be12a2d882c1830c71f61d770085f532eff
dev-spectre <abhishekdallasalpy@gmail.com>
2025-07-12 10:41:25 +0530

    update

---
+++ src/components/nav.jsx
@@ -0,0 +1,16 @@
+export default function NavBar() {
+  return (
+    <div class="navbar">
+      <div className="logo">Skill Swap</div>
+      <div>
+        <nav>
+          <a href=""></a>
+          <a href=""></a>
+        </nav>
+        <div className="profile">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
